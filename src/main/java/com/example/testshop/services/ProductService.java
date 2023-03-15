4b98e3e4ea74fcbbdab277f3d95dcffc36a3f462
georgiyburmistrov <burmistrov.nsgsm@gmail.com>
2023-03-15 22:05:24 +0400

    LOGs changed

--- src/main/java/com/example/testshop/services/ProductService.java
+++ src/main/java/com/example/testshop/services/ProductService.java
@@ -17,25 +17,25 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     public List<Product> listProducts(String title){
-        log.info("!!!!!!!!!!!!!!!!!!!!!!!!", title);
+        log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!", title);
         if (title != null)
             return productRepository.findByTitle(title);
         return productRepository.findAll();
     }
 
     public void saveProduct(Product product){
-        log.info("***********************", product);
+        log.info("**************************", product);
         // log.info("Saving new {}", product);
         productRepository.save(product);
     }
 
     public void deleteProduct(Long id){
-        log.info(">>>>>>>>>>>>>>>>>>>>>>>>>", id);
+        log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>", id);
         productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        log.info("+++++++++++++++++++++++++", id);
+        log.info("++++++++++++++++++++++++++++", id);
         return productRepository.findById(id).orElse(null);
     }
 }
