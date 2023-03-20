54f6e72c80e71e8e3758cf738759fa00a6ff657b
georgiyburmistrov <burmistrov.nsgsm@gmail.com>
2023-03-20 21:20:11 +0400

    Add fields for uploading photos

--- src/main/java/com/example/testshop/services/ProductService.java
+++ src/main/java/com/example/testshop/services/ProductService.java
@@ -1,12 +1,15 @@
 package com.example.testshop.services;
 
+import com.example.testshop.models.Image;
 import com.example.testshop.models.Product;
 import com.example.testshop.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,25 +20,52 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     public List<Product> listProducts(String title){
-        log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!", title);
+        log.info("Presentation of the entire list of products {}", title);
         if (title != null)
             return productRepository.findByTitle(title);
         return productRepository.findAll();
     }
 
-    public void saveProduct(Product product){
-        log.info("**************************", product);
-        // log.info("Saving new {}", product);
+    public void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException{
+        Image image1;
+        Image image2;
+        Image image3;
+        if (file1.getSize() != 0) {
+            image1 = toImageEntity(file1);
+            image1.setPreviewImage(true);
+            product.addImageToProduct(image1);
+        }
+        if (file2.getSize() != 0) {
+            image2 = toImageEntity(file2);
+            product.addImageToProduct(image2);
+        }
+        if (file3.getSize() != 0) {
+            image3 = toImageEntity(file3);
+            product.addImageToProduct(image3);
+        }
+        log.info("Saving new product Product. Title: {}; Salesman: {}", product.getTitle(), product.getSalesman());
+        Product productFromDB = productRepository.save(product);
+        productFromDB.setPreviewImageId(productFromDB.getImages().get(0).getId());
         productRepository.save(product);
     }
 
+    private Image toImageEntity(MultipartFile file) throws IOException {
+        Image image = new Image();
+        image.setName(file.getName());
+        image.setOriginalFileName(file.getOriginalFilename());
+        image.setContentType(file.getContentType());
+        image.setSize(file.getSize());
+        image.setBytes(file.getBytes());
+        return image;
+    }
+
     public void deleteProduct(Long id){
-        log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>", id);
+        log.info("Delete product {}", id);
         productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        log.info("++++++++++++++++++++++++++++", id);
+        log.info("Product presentation by ID {}", id);
         return productRepository.findById(id).orElse(null);
     }
 }
